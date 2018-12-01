647249af3bf4774d0e50b443b182443ef4b48e8d
mauriciocap@gmail.com <mauriciocap@gmail.com>
2018-12-01 09:36:52 -0300

    COMENTARIOSwq

---
+++ README.md
@@ -0,0 +1,3 @@
+//INFO: api rest para db relacional, ejemplo 1
+//ASK: https://pizarra.podemosaprender.org/2018/04/contacto.html
+//ASK: aprender@mauriciocap.com
--- app.js
+++ app.js
@@ -1,39 +1,44 @@
+//INFO: api rest para db relacional, ejemplo 1
+//ASK: https://pizarra.podemosaprender.org/2018/04/contacto.html
+//ASK: aprender@mauriciocap.com
+
 var Sequelize = require('sequelize')
   , finale = require('finale-rest')
   , http = require('http')
   , db      = require('./models')
 	;
+//A: cargue modulos que necesito
 
-// Initialize server
 var server, app;
 var express = require('express'),
 		bodyParser = require('body-parser');
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000); //A: si me pasan PORT= tengo que usar ese (ej. heroku)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 server = http.createServer(app);
+//A: configure servidor web
 
-// Initialize finale
 finale.initialize({
   app: app,
   sequelize: db.sequelize
 });
+//A: conecte finale (que atiende pedidos rest) al servidor web y mi db
 
-// Create REST resource
 var userResource = finale.resource({
   model: db.User,
   endpoints: ['/users', '/users/:id']
 });
+//A: cree urls (endpoints) REST para mi tabla de usuarios 
 
-// Create database and listen
 db.sequelize
-  .sync({ force: true })
+  .sync({ force: true })//A: actualice db segun modelos
   .then(function() {
     server.listen(app.get('port'),function() {
       var host = server.address().address,
           port = server.address().port;
       console.log('listening at http://%s:%s', host, port);
     });
-});
+}); //A: lance servidor y mostre en que puerto
+
