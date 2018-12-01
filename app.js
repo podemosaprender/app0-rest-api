b83c958b3ca1b0cb08300e582955868df87cba4d
mauriciocap@gmail.com <mauriciocap@gmail.com>
2018-12-01 09:20:12 -0300

    FIX: port

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var express = require('express'),
 		bodyParser = require('body-parser');
 
 var app = express();
+app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 server = http.createServer(app);
